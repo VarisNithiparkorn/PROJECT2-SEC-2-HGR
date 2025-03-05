dbd9ee33534adcc51dfcc1d96718afc3a5a3b38d
Varis <varisnithiparkorn@gmail.com>
2025-03-05 20:19:40 +0700

    105[add]-login layout

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,27 @@
 </script>
 
 <template>
-    <h1>
-        Ryu Branch
-    </h1>
-</template>
+    <div>
+      <div class=" flex items-center justify-center w-screen h-screen">
+        <div class=" max-[2561px]:max-w-[1960px] max-[2561px]:max-h-[1260px] max-[2561px]:flex-row flex w-full h-full border p-4">
+          <div class=" grow border p-4 flex flex-col justify-center items-center">
+            <div class=" flex flex-col w-11/12 h-11/12 border">
+              <h1 class=" text-8xl">
+                Shopping gun
+              </h1>
+            </div>
+          </div>
+          <div class=" grow border p-4 flex flex-col justify-center items-center">
+            <div class=" flex flex-col w-11/12 h-11/12 border">
+              <h1 class=" text-8xl">
+                Shopping gun
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
 
 <style scoped>
 
