c0a7968a2d32774d88f4124c843f99c352664473
Varis <varisnithiparkorn@gmail.com>
2025-03-12 21:29:27 +0700

    105[update] LoginForm SignUpForm UserInput and UserRegister lastest version

---
+++ src/components/UserRegister.vue
@@ -0,0 +1,19 @@
+<script setup>
+import LoginForm from '@/components/LoginForm.vue';
+import SignUpForm from '@/components/SignUpForm.vue';
+
+</script>
+
+<template>
+    <div>
+
+    <LoginForm>
+
+    </LoginForm>
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
