674791f993b45f5574607a6d68813e51840dc3d5
Varis <varisnithiparkorn@gmail.com>
2025-03-12 22:03:28 +0700

    105-[add] LoginForm SignUpForm UserInput UserRegister

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,20 @@
+<script setup>
+import UserInput from './UserInput.vue';
+const user = {firstName:null, lastName:null, username:null, password:null, email:null}
+</script>
+<template>
+    <div>
+        <div class= "w-screen h-screen flex flex-col items-center justify-center">
+            <div class=" w-[30%] h-[60%] flex flex-row flex-wrap items-center justify-center border">
+                <h2 class="text-4xl w-full text-center">
+                Sign up
+            </h2>
+            <UserInput :user="user">
+
+            </UserInput>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+</style>
---
+++ src/components/UserRegister.vue
@@ -0,0 +1,22 @@
+<script setup>
+import LoginForm from '@/components/LoginForm.vue';
+import SignUpForm from '@/components/SignUpForm.vue';
+
+</script>
+
+<template>
+    <div>
+
+    <LoginForm>
+
+    </LoginForm>
+    <SignUpForm>
+        
+    </SignUpForm>
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
