c7370dbb71db59d5d244a012b83c916173440d93
Varis <varisnithiparkorn@gmail.com>
2025-03-12 20:32:56 +0700

    105-[edit] import path in user Register

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-
+import UserRegister from './components/UserRegister.vue';
 
 </script>
 
 <template>
-
+    <UserRegister>
+        
+    </UserRegister>
 </template>
 
 <style scoped>
--- src/components/UserRegister.vue
+++ src/components/UserRegister.vue
@@ -1,16 +1,17 @@
 <script setup>
-import LoginForm from './components/LoginForm.vue';
-import SignUpForm from './components/SignUpForm.vue';
+import LoginForm from '@/components/LoginForm.vue';
+import SignUpForm from '@/components/SignUpForm.vue';
 
 </script>
 
 <template>
+    <div>
+
     <LoginForm>
 
     </LoginForm>
-    <SignUpForm>
 
-    </SignUpForm>
+</div>
 </template>
 
 <style scoped>
