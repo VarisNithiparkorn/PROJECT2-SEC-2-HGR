9f0392cf109d5836dbfb749312b0ddc823d40465
Phusara <pozzuoli97@gmail.com>
2025-03-13 22:19:21 +0700

    124-[Update] Update UI list item on main page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
+import { ref } from "vue";
 import Header from "./components/Header.vue";
+import productSection from "./components/productSection.vue";
+import dataFile from '../data/db.json';
+const data = ref(dataFile.products)
+console.log(data.value);
 </script>
 
 <template>
   <!-- show , hide -->
   <Header navBar="show" />
-
+  <div class="flex flex-wrap gap-3 justify-center pt-20">
+  <productSection v-for="item in data" :key="item.id" :item="item" />
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/productSection.vue
+++ src/components/productSection.vue
@@ -1,28 +1,24 @@
 <script setup>
-import { ref } from "vue";
-import dataFile from '../../data/db.json';
 import { defineProps } from 'vue';
-const data = ref(dataFile)
+
 const props = defineProps({
   item: {
     type: Object,
     required: true
   }
 });
-for (const { ID, name, price, brand } of data.value) {
-  console.log(name);
-}
 </script>
 
 <template>
-   <div class="flex h-36 w-32 bg-white text-black border-solid border-red-500 border-2">
+  <div class="flex h-36 w-32 bg-white text-black border-solid border-red-500 border-2">
     <div class="text-center h-36 w-32">
-    <p class="text-xl">{{ item.name }}</p>
-    <p class="text-lg">{{ item.brand }}</p>
-    <p class="text-base">{{ item.price }}</p>
-</div>
+      <p class="text-xl">{{ item.productName }}</p>
+      <p class="text-lg">{{ item.price }}</p>
+      <p class="text-base">{{ item.quantityInStock }}</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
+
 </style>
