ffc716602c7440840130e39360fe37602cb8da43
Phusara <pozzuoli97@gmail.com>
2025-03-15 20:27:53 +0700

    124-[Add] Add simple test ui for filter, [Update] Update Product list page

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import Header from "./components/Header.vue";
 import productSection from "./components/productSection.vue";
 import dataFile from '../data/db.json';
+import FillerProduct from "./components/FillerProduct.vue";
 const data = ref(dataFile.products)
 console.log(data.value);
 </script>
@@ -10,9 +11,17 @@ console.log(data.value);
 <template>
   <!-- show , hide -->
   <Header navBar="show" />
-  <div class="flex flex-wrap gap-3 justify-center pt-20">
-  <productSection v-for="item in data" :key="item.id" :item="item" />
+  
+  <div class=" flex pt-20">
+  <div class="inset-y-0 left-0">
+  <FillerProduct/> 
   </div>
+  <div class="flex flex-wrap gap-3 justify-center">
+  <productSection v-for="item in data" :key="item.id" :item="item" />
+</div>
+  </div>  
+  
+
 </template>
 
 <style scoped></style>
