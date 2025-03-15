bf9ee09a14c8900c25cb644f9e910b8a3acac430
Kritsada Atchariyapruk <kritsada.gun123@gmail.com>
2025-03-15 11:42:07 +0700

    109-[Update] show count of item in cart & search product name from db in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,32 +8,51 @@ const props = defineProps({
     },
     default: "show",
   },
+  userId: {
+    type: Number,
+    required: true,
+  },
 });
 const searchText = ref("");
-const text = ["computer", "laptop", "mouse", "cpu", "gpu", "keyboard"];
+const products = ref([]);
+const productName = ref([]);
 
 const searchMatch = computed(() => {
-  return text.filter((t) => {
+  return productName.value.filter((p) => {
     if (
-      t.charAt(0).toLowerCase() === searchText.value.charAt(0).toLowerCase()
+      p.charAt(0).toLowerCase() === searchText.value.charAt(0).toLowerCase()
     ) {
-      return t.toLowerCase().includes(searchText.value.toLowerCase());
+      return p.toLowerCase().includes(searchText.value.toLowerCase());
     }
   });
 });
 
-const cart = ref([])
-const itemInCart = computed(() => cart.value.length + 1);
-onMounted(async () =>{
-  try{
-    const item = await getItems(`${import.meta.env.VITE_APP_URL}/carts`)
-    cart.value.push(item)
-    itemInCart.value = cart.value.length 
-    console.log(itemInCart.value); 
-  }catch(error){
+const carts = ref([]);
+const itemCountInCart = computed(() => {
+  let count = 0;
+  for (const cart of carts.value) {
+    let cartId = Number(cart.id);
+    if (props.userId === cartId) {
+      cart.products.forEach((p) => {
+        count += p.amount;
+      });
+    }
+    return count;
+  }
+});
+onMounted(async () => {
+  try {
+    const item = await getItems(`${import.meta.env.VITE_APP_URL}/carts`);
+    const product = await getItems(`${import.meta.env.VITE_APP_URL}/products`);
+    carts.value = Array.isArray(item) ? item : [item];
+    products.value = Array.isArray(product) ? product : [product];
+    for (const product of products.value) {
+      productName.value.push(product.productName);
+    }
+  } catch (error) {
     console.log(error);
   }
-})
+});
 </script>
 
 <template>
@@ -104,7 +123,9 @@ onMounted(async () =>{
                 d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
               />
             </svg>
-            <span class="badge badge-sm indicator-item">{{ itemInCart }}</span>
+            <span class="badge badge-sm indicator-item">{{
+              itemCountInCart
+            }}</span>
           </div>
         </div>
       </div>
