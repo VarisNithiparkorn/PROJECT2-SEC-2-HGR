d5fbf347b44c8d32c06730c177deaf482ae938a7
Phusara <pozzuoli97@gmail.com>
2025-03-15 20:27:53 +0700

    124-[Add] Add simple test ui for filter, [Update] Update Product list page

--- src/components/FillerProduct.vue
+++ src/components/FillerProduct.vue
@@ -3,7 +3,26 @@
 </script>
 
 <template>
+    
 
+<div class="w-62 h-auto bg-amber-300 p-4">
+    <div>
+    <select class="select">
+  <option disabled selected>Pick a color</option>
+  <option>Crimson</option>
+  <option>Amber</option>
+  <option>Velvet</option>
+</select>
+</div>
+    <div>
+    <input type="checkbox" checked="checked" class="checkbox checkbox-xs" id="test1" />
+    <label for="test1">test1</label><br>
+    <input type="checkbox" checked="checked" class="checkbox checkbox-xs" id="test2" />
+    <label for="test2">test2</label><br>
+   <input type="checkbox" checked="checked" class="checkbox checkbox-xs" id="test3"/>
+   <label for="test3">test3</label><br>
+  </div>
+  </div>
 </template>
 
 <style scoped>
