94e9d570045fc5694b2e4641185b366355ab6ae2
Varis <varisnithiparkorn@gmail.com>
2025-03-21 08:59:22 +0700

    105-[update]restructure all user component

---
+++ src/components/UserAccountManger.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+import CreateOrAuthAccount from './CreateOrAuthAccount.vue';
+import AccountInfo from './AccountInfo.vue';
+const prop = defineProps({
+  currentAccount:{
+    type:Object,
+    default:{}
+  }
+})
+const currentAccount = ref(prop.currentAccount)
+function setAccount(loginAcc) {
+    currentAccount.value = loginAcc
+}
+</script>
+<template>
+  <div>
+  <CreateOrAuthAccount v-show="false" :hasUser="JSON.stringify(currentAccount) === '{}'? false: true" @login="setAccount">
+
+  </CreateOrAuthAccount>
+  <AccountInfo v-show="false">
+
+  </AccountInfo>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
