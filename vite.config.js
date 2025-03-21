6d49fe6b4ca86eb77b25e892626837feb209aa8f
Kritsada Atchariyapruk <kritsada.gun123@gmail.com>
2025-03-21 11:00:05 +0700

    rebase lastest update

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  server: {
-    hmr: false, 
-  },  
+  // server: {
+  //   hmr: false, 
+  // },  
 })
