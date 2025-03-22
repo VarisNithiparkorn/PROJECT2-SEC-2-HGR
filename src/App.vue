18682dc645ddb93d01fd78486fce6f4143de42f0
Phusara <pozzuoli97@gmail.com>
2025-03-22 15:16:48 +0700

    124 [Add,Update,Remove] Add Product detail page, Add Function Fillter Price,Quantity and Item, Update Productsection page with Fillter, Remove FillterProduct.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { ref } from "vue";
 import Header from "./components/Header.vue";
 import productSection from "./components/productSection.vue";
 import dataFile from '../data/db.json';
-import FillerProduct from "./components/FillerProduct.vue";
+import productDetails from "./components/productDetails.vue";
 const data = ref(dataFile.products)
 console.log(data.value);
 </script>
@@ -12,17 +12,12 @@ console.log(data.value);
   <!-- show , hide -->
   <Header navBar="show" :userId="1"/>
   <!-- List product and filter -->
-  <div class="grid grid-cols-6 h-full">
-  <div class="h-max  col-start-1 row-span-2">
-  <FillerProduct/> 
-  </div>
-  <div class=" col-start-2 col-end-7 pt-10">
-  <div class="flex flex-1 flex-wrap gap-3 pl-10">
-  <productSection v-for="item in data" :key="item.id" :item="item" />
+<div>
+  <productSection/>
 </div>
-</div>
-</div>  
-
+<!-- <div class="md:pt-20">
+<productDetails/>
+</div> -->
 </template>
 
 <style scoped></style>
